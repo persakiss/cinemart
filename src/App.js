8897b49a7fb2f2536068e0911bc707e561eea71f
Arman <persakiss@gmail.com>
2022-06-28 14:23:30 +0400

    full height banner, removed nav, preparing for page scroll parallax

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
 import "./App.scss";
 import Header from "./components/Header";
-import { MDBContainer, MDBRow, MDBCol, MDBTypography } from "mdbreact";
 
-import bg1 from "./assets/img/header/background.png";
-import fg1 from "./assets/img/header/foreground.png";
-import lady from "./assets/img/header/lady.png";
+import ReactPageScroller from 'react-page-scroller';
+import FirstPage from "./components/FirstPage";
+
 
 export default function App() {
   return (
     <>
-      <Header />
       <ParallaxProvider>
-        <ParallaxBanner
-          layers={[
-            { image: bg1, speed: -10 },
-            { translateX: [ 19, 0 ], translateY: [18, 40],
-              speed: -5,
-              children: (
-                <MDBContainer>
-                  <MDBRow>
-                    <MDBCol>
-                      <MDBTypography
-                        tag="div"
-                        className="display-1 pb-3 mb-3 text-white header-text"
-                      >
-                        cinemart
-                      </MDBTypography>
-                      <MDBTypography
-                        tag="div"
-                        className="display-6 pb-3 mb-3 text-white sub-text"
-                      >
-                        action through theater
-                      </MDBTypography>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBContainer>
-              ),
-            },
-            { image: fg1, speed: 5 },
-          ]}
-          className="aspect-[2/1] header-top "
-        ></ParallaxBanner>
+        <FirstPage/>
       </ParallaxProvider>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
     </>
   );
 }
---
+++ src/components/FirstPage.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { MDBContainer, MDBRow, MDBCol, MDBTypography } from "mdbreact";
+import { ParallaxBanner } from 'react-scroll-parallax';
+import bg1 from "../assets/img/header/background.png";
+import fg1 from "../assets/img/header/foreground.png";
+
+function FirstPage() {
+  return (
+    
+    <ParallaxBanner
+    layers={[
+      { image: bg1, speed: -10 },
+      {
+        translateX: [10, 0],
+        translateY: [18, 40],
+        speed: -10,
+        children: (
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol>
+                {/* Large+ */}
+                <div class="d-none d-xl-block d-xl-none">
+                  <MDBTypography
+                    tag="div"
+                    className="display-1 pb-3 mb-3 text-white header-text"
+                  >
+                    cinemart
+                  </MDBTypography>
+                  <MDBTypography
+                    tag="div"
+                    className="display-6 pb-3 mb-3 text-white sub-text"
+                  >
+                    action through theater
+                  </MDBTypography>
+                </div>
+                <div class="d-none d-sm-block d-md-none">
+                <MDBTypography
+                  tag="div"
+                  className="display-1 pb-3 mb-3 text-white header-text"
+                >
+                  cinemart
+                </MDBTypography>
+                <MDBTypography
+                  tag="div"
+                  className="display-6 pb-3 mb-3 text-white sub-text "
+                >
+                  action through theater
+                </MDBTypography>
+                </div>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        ),
+      },
+      { image: fg1, speed: 5 },
+    ]}
+    className="aspect-[2/1] header-top "
+  ></ParallaxBanner>
+  )
+}
+
+export default FirstPage
